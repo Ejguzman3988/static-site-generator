c977c1da0c491358560eb0ed2c25e5d2c2c71572
Eriberto Guzman <eriberto.guzman@outlook.com>
2025-06-03 01:58:20 -0400

    update

--- src/markdown.py
+++ src/markdown.py
@@ -1,3 +1,13 @@
+from blocktype import PARAGRAPH, block_to_block_type
+
+
+def markdown_to_html_node(markdown):
+    blocks = markdown_to_blocks(markdown)
+
+    for block in blocks:
+        block_type = block_to_block_type(block)
+
+
 def markdown_to_blocks(markdown):
     return list(
         filter(lambda x: x != "", map(lambda x: x.strip(), markdown.split("\n\n")))
